d2bdc8b9caefa2f263bd8ee1f9879c1824722505
Luis Diaz <v-lureynoso@expediagroup.com>
2024-08-12 18:06:15 -0700

    Initializing

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, "src", "index.js"),
+    output: {
+      path:path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, "public", "index.html"),
+      }),
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react']
+              }
+            }
+          },
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(jpe?g|png|gif|svg)$/i, 
+            loader: 'file-loader',
+          }
+        ]
+      },
+  devServer: {
+    port: 4000,
+  },
+};
